1ebe9fb04278169cf54e5b1ac833f5032145a84f
daniel-chu <chu.da@husky.neu.edu>
2017-07-18 22:07:45 -0400

    Implemented page and widget services.

--- public/assignment/services/page.service.client.js
+++ public/assignment/services/page.service.client.js
@@ -1,3 +1,68 @@
-/**
- * Created by danielchu on 7/18/17.
- */
+(function () {
+    angular
+        .module("WebAppMaker")
+        .factory("pageService", pageService);
+
+    function pageService() {
+        var pages = [
+            {"_id": "321", "name": "Post 1", "websiteId": "456", "description": "Lorem"},
+            {"_id": "432", "name": "Post 2", "websiteId": "456", "description": "Lorem"},
+            {"_id": "543", "name": "Post 3", "websiteId": "456", "description": "Lorem"}
+        ];
+
+        var api = {
+            createPage: createPage,
+            findPagesByWebsiteId: findPagesByWebsiteId,
+            findPageById: findPageById,
+            updatePage: updatePage,
+            deletePage: deletePage
+        }
+
+        return api;
+
+        function createPage(websiteId, page) {
+            page.websiteId = websiteId;
+            pages.push(page);
+        }
+
+        function findPagesByWebsiteId(websiteId) {
+            var pagesInWebsite = [];
+
+            for (var i = 0; i < pages.length; i++) {
+                var curPage = pages[i];
+                if (curPage.websiteId == websiteId) {
+                    pagesInWebsite.push(curPage);
+                }
+            }
+
+            return pagesInWebsite;
+        }
+
+        function findPageById(pageId) {
+            for (var i = 0; i < pages.length; i++) {
+                if (pages[i]._id == pageId) {
+                    return pages[i];
+                }
+            }
+
+            return null;
+        }
+
+        function updatePage(pageId, page) {
+            for (var i = 0; i < pages.length; i++) {
+                if (pages[i]._id == pageId) {
+                    pages[i] = page;
+                }
+            }
+        }
+
+        function deletePage(pageId) {
+            for (var i = 0; i < pages.length; i++) {
+                if (pages[i]._id == pageId) {
+                    pages.splice(i, 1);
+                }
+            }
+        }
+    }
+
+})();
--- public/assignment/services/widget.service.client.js
+++ public/assignment/services/widget.service.client.js
@@ -1,3 +1,110 @@
-/**
- * Created by danielchu on 7/18/17.
- */
+(function () {
+    angular
+        .module("WebAppMaker")
+        .factory("widgetService", widgetService);
+
+    function widgetService() {
+        var widgets = [
+            {
+                "_id": "123",
+                "widgetType": "HEADING",
+                "pageId": "321", "size": 2,
+                "text": "GIZMODO"
+            },
+            {
+                "_id": "234",
+                "widgetType": "HEADING",
+                "pageId": "321", "size": 4,
+                "text": "Lorem ipsum"
+            },
+            {
+                "_id": "345",
+                "widgetType": "IMAGE",
+                "pageId": "321",
+                "width": "100%",
+                "url": "http://lorempixel.com/400/200/"
+            },
+            {
+                "_id": "456",
+                "widgetType": "HTML",
+                "pageId": "321",
+                "text": "<p>Lorem ipsum</p>"
+            },
+            {
+                "_id": "567",
+                "widgetType": "HEADING",
+                "pageId": "321", "size": 4,
+                "text": "Lorem ipsum"
+            },
+            {
+                "_id": "678",
+                "widgetType": "YOUTUBE",
+                "pageId": "321",
+                "width": "100%",
+                "url": "https://youtu.be/AM2Ivdi9c4E"
+            },
+            {
+                "_id": "789",
+                "widgetType": "HTML",
+                "pageId": "321",
+                "text": "<p>Lorem ipsum</p>"
+            }
+        ];
+
+
+        var api = {
+            createWidget: createWidget,
+            findWidgetsByPageId: findWidgetsByPageId,
+            findWidgetById: findWidgetById,
+            updateWidget: updateWidget,
+            deleteWidget: deleteWidget
+        }
+
+        return api;
+
+        function createWidget(pageId, widget) {
+            widget.pageId = pageId;
+            widgets.push(widget);
+        }
+
+        function findWidgetsByPageId(pageId) {
+            var widgetsInPage = [];
+
+            for (var i = 0; i < widgets.length; i++) {
+                var curWidget = widgets[i];
+                if (curWidget.pageId == pageId) {
+                    widgetsInPage.push(curWidget);
+                }
+            }
+
+            return widgetsInPage;
+        }
+
+        function findWidgetById(widgetId) {
+            for (var i = 0; i < widgets.length; i++) {
+                if (widgets[i]._id == widgetId) {
+                    return widgets[i];
+                }
+            }
+
+            return null;
+        }
+
+        function updateWidget(widgetId, widget) {
+            for (var i = 0; i < widgets.length; i++) {
+                if (widgets[i]._id == widgetId) {
+                    widgets[i] = widget;
+                }
+            }
+        }
+
+        function deleteWidget(widgetId) {
+            for (var i = 0; i < widgets.length; i++) {
+                if (widgets[i]._id == widgetId) {
+                    widgets.splice(i, 1);
+                }
+            }
+        }
+    }
+
+})();
