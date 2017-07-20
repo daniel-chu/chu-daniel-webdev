741b25194a0727374c3c50401f497ae856700b3d
daniel-chu <chu.da@husky.neu.edu>
2017-07-20 14:30:48 -0400

    implemented new widget functionality, modified widget service to generate ids upon creation. changed widget service functions to return the widget they create/update/delete.

--- public/assignment/services/widget.service.client.js
+++ public/assignment/services/widget.service.client.js
@@ -4,6 +4,10 @@
         .factory("widgetService", widgetService);
 
     function widgetService() {
+        // temporary counter for id generation before we implement database. starts at 1000 because there are
+        // already some in the given websites array
+        var idCounter = 1000;
+
         var widgets = [
             {
                 "_id": "123",
@@ -11,42 +15,36 @@
                 "pageId": "321",
                 "size": 2,
                 "text": "GIZMODO"
-            },
-            {
+            }, {
                 "_id": "234",
                 "widgetType": "HEADING",
                 "pageId": "321",
                 "size": 4,
                 "text": "Lorem ipsum"
-            },
-            {
+            }, {
                 "_id": "345",
                 "widgetType": "IMAGE",
                 "pageId": "321",
                 "width": "100%",
                 "url": "http://lorempixel.com/400/200/"
-            },
-            {
+            }, {
                 "_id": "456",
                 "widgetType": "HTML",
                 "pageId": "321",
                 "text": "<p>Lorem ipsum</p>"
-            },
-            {
+            }, {
                 "_id": "567",
                 "widgetType": "HEADING",
                 "pageId": "321",
                 "size": 4,
                 "text": "Lorem ipsum"
-            },
-            {
+            }, {
                 "_id": "678",
                 "widgetType": "YOUTUBE",
                 "pageId": "321",
                 "width": "100%",
                 "url": "https://youtu.be/AM2Ivdi9c4E"
-            },
-            {
+            }, {
                 "_id": "789",
                 "widgetType": "HTML",
                 "pageId": "321",
@@ -66,8 +64,10 @@
         return api;
 
         function createWidget(pageId, widget) {
+            widget._id = idCounter++;
             widget.pageId = pageId;
             widgets.push(widget);
+            return widget;
         }
 
         function findWidgetsByPageId(pageId) {
@@ -99,12 +99,13 @@
                     widgets[i] = widget;
                 }
             }
+            return widget;
         }
 
         function deleteWidget(widgetId) {
             for (var i = 0; i < widgets.length; i++) {
                 if (widgets[i]._id == widgetId) {
-                    widgets.splice(i, 1);
+                    return widgets.splice(i, 1);
                 }
             }
         }
--- public/assignment/views/widget/controllers/widget-new.controller.client.js
+++ public/assignment/views/widget/controllers/widget-new.controller.client.js
@@ -1,10 +1,41 @@
-(function () {
+(function() {
     angular
         .module('WebAppMaker')
         .controller('newWidgetController', newWidgetController);
 
-    function newWidgetController($routeParams, $location) {
+    function newWidgetController($routeParams, $location, widgetService) {
         var vm = this;
+        vm.userId = $routeParams['uid'];
+        vm.websiteId = $routeParams['wid'];
+        vm.pageId = $routeParams['pid'];
+        vm.createHeadingWidget = createHeadingWidget;
+        vm.createImageWidget = createImageWidget;
+        vm.createYoutubeWidget = createYoutubeWidget;
+
+        function createHeadingWidget() {
+            var newWidget = widgetService.createWidget(vm.pageId, {
+                widgetType: 'HEADING'
+            });
+            $location.url('/user/' + vm.userId + '/website/' + vm.websiteId + '/page/'
+                + vm.pageId + '/widget/' + newWidget._id);
+        }
+
+        function createImageWidget() {
+            widgetService.createWidget(vm.pageId, {
+                widgetType: 'IMAGE'
+            });
+            $location.url('/user/' + vm.userId + '/website/' + vm.websiteId + '/page/'
+                + vm.pageId + '/widget/' + newWidget._id);
+        }
+
+        function createYoutubeWidget() {
+            widgetService.createWidget(vm.pageId, {
+                widgetType: 'YOUTUBE'
+            });
+            $location.url('/user/' + vm.userId + '/website/' + vm.websiteId + '/page/'
+                + vm.pageId + '/widget/' + newWidget._id);
+        }
+
     }
 
     //TODO WIDGET-NEW NEEDS TO BE DONE
