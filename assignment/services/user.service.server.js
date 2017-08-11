26f78b9787f6ef7fe2ebcaf54f4368f06f2bef05
daniel-chu <chu.da@husky.neu.edu>
2017-08-11 16:38:26 -0400

    Used custom callback for login, removed unneeded comments.

--- assignment/services/user.service.server.js
+++ assignment/services/user.service.server.js
@@ -3,50 +3,66 @@ var UserModel = require('../model/user/user.model.server.js');
 
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var auth = authorized;
 
 passport.use(new LocalStrategy(localStrategy));
 passport.serializeUser(serializeUser);
 passport.deserializeUser(deserializeUser);
-
-// { _id: "123", username: "alice", password: "alice", firstName: "Alice", lastName: "Wonder" },
-// { _id: "234", username: "bob", password: "bob", firstName: "Bob", lastName: "Marley" },
-// { _id: "345", username: "charly", password: "charly", firstName: "Charly", lastName: "Garcia" },
-// { _id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose", lastName: "Annunzi" }
-
 app.post('/api/user', createUser);
 app.get('/api/user', findUserByUsername)
-app.post('/api/login', passport.authenticate('local'), login);
 app.post('/api/logout', logout);
 app.get('/api/checkLogin', checkLogin);
 app.get('/api/user/:userId', findUserById);
-app.put('/api/user/:userId', updateUser);
-app.delete('/api/user/:userId', deleteUser);
+app.put('/api/user/:userId', auth, updateUser);
+app.delete('/api/user/:userId', auth, deleteUser);
+
+app.post('/api/login', function (req, res) {
+    passport.authenticate('local', function (err, user, info) {
+        if (!user) {
+            return res.send(null);
+        }
+        req.logIn(user, function (err) {
+            return res.send(user);
+        });
+    })(req, res);
+});
+
+function authorized(req, res, next) {
+    if (!req.isAuthenticated()) {
+        res.sendStatus(401);
+    } else {
+        next();
+    }
+};
 
 function createUser(req, res) {
     var user = req.body.user;
 
-    UserModel.createUser(user).then(function(newUser) {
-        res.send(newUser);
-    });
+    UserModel.createUser(user)
+        .then(function (newUser) {
+            req.logIn(newUser, function (err) {
+                res.send(newUser);
+            });
+        });
 }
 
 function findUserByUsername(req, res) {
     var username = req.query.username;
 
-    UserModel.findUserByUsername(username).then(function(user) {
+    UserModel.findUserByUsername(username).then(function (user) {
         res.send(user);
     });
 }
 
 function localStrategy(username, password, done) {
-    UserModel.findUserByCredentials(username, password)
-        .then(function(user) {
-                if (!user) {
-                    return done(null, false);
+    UserModel.findUserByUsername(username, password)
+        .then(function (user) {
+                if (user && password === user.password) {
+                    return done(null, user);
                 }
-                return done(null, user);
+                return done(null, false);
             },
-            function(err) {
+            function (err) {
                 if (err) {
                     return done(err);
                 }
@@ -71,7 +87,7 @@ function checkLogin(req, res) {
 function findUserById(req, res) {
     var userId = req.params.userId;
 
-    UserModel.findUserById(userId).then(function(user) {
+    UserModel.findUserById(userId).then(function (user) {
         res.send(user);
     });
 }
@@ -80,7 +96,7 @@ function updateUser(req, res) {
     var userId = req.params.userId;
     var user = req.body.user;
 
-    UserModel.updateUser(userId, user).then(function(status) {
+    UserModel.updateUser(userId, user).then(function (status) {
         res.sendStatus(204);
     });
 }
@@ -88,7 +104,7 @@ function updateUser(req, res) {
 function deleteUser(req, res) {
     var userId = req.params.userId;
 
-    UserModel.deleteUser(userId).then(function(status) {
+    UserModel.deleteUser(userId).then(function (status) {
         res.sendStatus(204);
     });
 }
@@ -101,10 +117,10 @@ function deserializeUser(id, done) {
     UserModel
         .findUserById(id)
         .then(
-            function(user) {
+            function (user) {
                 done(null, user);
             },
-            function(err) {
+            function (err) {
                 done(err, null);
             }
         );
--- assignment/services/website.service.server.js
+++ assignment/services/website.service.server.js
@@ -1,14 +1,6 @@
 var app = require('../../express');
 var WebsiteModel = require('../model/website/website.model.server.js');
 
-// { "_id": "123", "name": "Facebook", "developerId": "456", "description": "Lorem" },
-// { "_id": "234", "name": "Tweeter", "developerId": "456", "description": "Lorem" },
-// { "_id": "456", "name": "Gizmodo", "developerId": "456", "description": "Lorem" },
-// { "_id": "890", "name": "Go", "developerId": "123", "description": "Lorem" },
-// { "_id": "567", "name": "Tic Tac Toe", "developerId": "123", "description": "Lorem" },
-// { "_id": "678", "name": "Checkers", "developerId": "123", "description": "Lorem" },
-// { "_id": "789", "name": "Chess", "developerId": "234", "description": "Lorem" }
-
 app.post('/api/user/:userId/website', createWebsite);
 app.get('/api/user/:userId/website', findWebsitesByUser);
 app.get('/api/website/:websiteId', findWebsiteById);
